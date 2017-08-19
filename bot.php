25d4f530bea894d54c354480d9df76c8911f024c
cpblue <cpblue@gmail.com>
2017-08-19 16:12:21 +0700

    remove original manual bot code

--- bot.php
+++ bot.php
@@ -27,33 +27,33 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-			];
+			// $messages = [
+			// 	'type' => 'text',
+			// 	'text' => $text
+			// ];
 
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+			// // Make a POST Request to Messaging API to reply to sender
+			// $url = 'https://api.line.me/v2/bot/message/reply';
+			// $data = [
+			// 	'replyToken' => $replyToken,
+			// 	'messages' => [$messages],
+			// ];
+			// $post = json_encode($data);
+			// $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-		      $proxy = 'velodrome.usefixie.com:80';
-		      $proxyauth = 'fixie:aViCdpri3CDEA4v';
-		      curl_setopt($ch, CURLOPT_PROXY, $proxy);
-		      curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
-			$result = curl_exec($ch);
-			curl_close($ch);
-			echo $result . "\r\n";
+			// $ch = curl_init($url);
+			// curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			// curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			// curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			// curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+		 //      $proxy = 'velodrome.usefixie.com:80';
+		 //      $proxyauth = 'fixie:aViCdpri3CDEA4v';
+		 //      curl_setopt($ch, CURLOPT_PROXY, $proxy);
+		 //      curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+			// $result = curl_exec($ch);
+			// curl_close($ch);
+			// echo $result . "\r\n";
 
 			$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
 			$response = $bot->replyMessage($replyToken, $textMessageBuilder);
